9a88a1fe825f08890e88de1b457bdc549ab665ff
Anders Alfredsson <andersb86@gmail.com>
2020-11-05 08:11:53 +0100

    Restructure calculation

--- 11_as1/circular_sector.py
+++ 11_as1/circular_sector.py
@@ -4,6 +4,6 @@ radius = float(input('Radie: ').replace(',', '.'))
 degrees = float(input('Grader: ').replace(',', '.'))
 
 full_circle_area = pi * radius ** 2
-sector_area = full_circle_area / 360 * degrees
+sector_area = full_circle_area * (degrees / 360)
 
 print('Cirkelsektorns area är {}'.format(sector_area))
