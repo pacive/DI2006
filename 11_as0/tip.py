422d4f617cccdf403b79ead8522c874d777ac7e4
Anders Alfredsson <andersb86@gmail.com>
2020-11-09 11:14:41 +0100

    Comvert to constant

--- 11_as0/tip.py
+++ 11_as0/tip.py
@@ -1,5 +1,7 @@
+TIP_RATIO = 0.15
+
 bill = float(input('Vad går notan på? ').replace(',', '.'))
 
-tip = bill * 0.15
+tip = bill * TIP_RATIO
 
 print('Dricksa {} kr (betala totalt {} kr)'.format(tip, bill + tip))
