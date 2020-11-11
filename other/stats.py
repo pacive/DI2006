3f0ec54ca0dd71a866ee0abef410219beb813e8d
Anders Alfredsson <andersb86@gmail.com>
2020-11-11 08:13:05 +0100

    Compact code and remove duplicated builtin sum()

--- other/stats.py
+++ other/stats.py
@@ -1,20 +1,17 @@
 import math
 
-def sum(array):
-    sum = 0
-    for n in array:
-        sum += n
-    return sum
-
 def mean(array):
     return sum(array) / len(array)
 
 def variance(array):
     m = mean(array)
-    diffs = []
-    for n in array:
-        diffs.append((n - m) ** 2)
+    diffs = [(n - m) ** 2 for n in array]
     return mean(diffs)
 
 def stddev(array):
     return math.sqrt(variance(array))
+
+arr = [5, 6, 9, 7, 5, 4, 6, 7, 2, 4]
+print(sum(arr))
+print(mean(arr))
+print(stddev(arr))
