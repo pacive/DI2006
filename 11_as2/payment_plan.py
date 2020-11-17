e5d69e50fc88f980937feb704a9067152269d030
pacive <andersb86@gmail.com>
2020-11-17 15:33:23 +0100

    Clarify input

--- 11_as2/payment_plan.py
+++ 11_as2/payment_plan.py
@@ -3,17 +3,15 @@ PRICE_FACTORS = { '1': 0.9, '2': 0.95, '3': 1, '4': 1.2 }
 
 price = float(input('Ange pris: ').replace(',', '.'))
 
-print('''
-Välj betalningssätt:
+print('''Välj betalningssätt:
 1. Kontant
 2. Kort
 3. 2 månaders avbetalning
-4. 3 månaders avbetalning
-''')
+4. 3 månaders avbetalning''')
 
 # Check that the selection is valid
-while (plan := input()) not in PRICE_FACTORS:
-    print('Ogiltigt val, försök igen: ', end='')
+while (plan := input('> ')) not in PRICE_FACTORS:
+    print('Ogiltigt val, försök igen')
 
 final_price = price * PRICE_FACTORS[plan]
 
