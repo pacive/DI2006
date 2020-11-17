e2dfd6e784070b967fb36a8becbdb2aea21de55d
Anders Alfredsson <andersb86@gmail.com>
2020-11-17 07:26:15 +0100

    Change order of calculations

--- 11_as2/prime.py
+++ 11_as2/prime.py
@@ -1,9 +1,6 @@
 import math
 
 def is_prime(num):
-    # Only need to test factors up to the sqare root of num
-    largest_factor = int(math.sqrt(num)) + 1
-
     # Check if num is 2 - the only even prime
     if num == 2:
         return True
@@ -11,6 +8,9 @@ def is_prime(num):
     # If num is even, it can't be a prime
     if num % 2 == 0:
         return False
+
+    # Only need to test factors up to the sqare root of num
+    largest_factor = int(math.sqrt(num)) + 1
 
     # If num is divisible by any other number, it's not prime
     for n in range(3, largest_factor, 2):
