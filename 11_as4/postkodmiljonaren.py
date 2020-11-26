309b60978b8730622d98c36f44ca7a1766421930
Anders Alfredsson <andersb86@gmail.com>
2020-11-26 19:19:40 +0100

    Various improvements

--- 11_as4/postkodmiljonaren.py
+++ 11_as4/postkodmiljonaren.py
@@ -1,5 +1,21 @@
 import os
 
+INSTRUCTIONS = '''Welcome to 'Who wants to be a millionaire'!
+
+This came consists of 15 questions, that gets progressively harder, but also
+comes with greater rewards.
+
+As long as you answer correctly you earn more money, but if you answer wrong, 
+you lose what you have earned.
+
+After questions 5 and 10 you get a guarantee that you will get at least that amount, even if
+you fail a later question.
+
+You are free to give up at any time (by answering with 'q') and you walk away with what you have earned.
+
+Press return to begin...
+'''
+
 class Question():
     def __init__(self, question, choices, correct_answer, prize):
         self.question = question
@@ -13,7 +29,7 @@ class Question():
             print(f'{i + 1}. {self.choices[i]}')
 
     def validate_answer(self, answer):
-        return answer - 1 == self.correct_answer
+        return int(answer) - 1 == self.correct_answer
 
     def get_correct(self):
         return self.choices[self.correct_answer]
@@ -26,7 +42,7 @@ def load_questions(directory = os.path.dirname(__file__)):
 
     questions = []
 
-    for i in range(15):
+    for _ in range(15):
         question = question_file.readline().strip()
         choices = []
         for _ in range(4):
@@ -46,22 +62,36 @@ def load_questions(directory = os.path.dirname(__file__)):
 def get_answer():
     while True:
         try:
-            answer = int(input('==> '))
-            if answer in range(1, 5):
+            answer = input('==> ')
+            if answer == 'q' or int(answer) in range(1, 5):
                 return answer
-            else:
-                print('Ogiltigt svar, försök igen')
+
+            print('Ogiltigt svar, försök igen')
         except ValueError:
             print('Ogiltigt svar, försök igen')
 
 def main():
-    questions = load_questions()
+    try:
+        questions = load_questions()
+    except (IOError, FileNotFoundError) as e:
+        print('Failed to load questions:', e)
+        exit(1)
+
     guaranteed = 0
+
+    input(INSTRUCTIONS)
+
     for q in range(15):
         current_question = questions[q]
+
         print(f'Question {q + 1} for {current_question.prize}')
         current_question.print()
         answer = get_answer()
+
+        if answer == 'q':
+            print(f'Thanks for playing, you get to take {questions[q-1].prize} home.')
+            break
+
         if current_question.validate_answer(answer):
             if q == 14:
                 print('YOU WON!!!!! YOU\'RE A MILLIONAIRE!!!!!!!!!!')
@@ -71,10 +101,12 @@ def main():
                 guaranteed = current_question.prize
         else:
             print('Sorry, that is the wrong answer, ', end='')
+
             if guaranteed == 0:
                 print('you will go home empty handed.')
             else:
                 print(f'you get to take {guaranteed} home.')
+
             print(f'The right answer was {current_question.get_correct()}')
             break
 
