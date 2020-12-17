b4430f9b63e2ad327e761ff51e940cb49669b950
Anders Alfredsson <andersb86@gmail.com>
2020-12-17 14:49:50 +0100

    Add comments

--- mastermind/main.py
+++ mastermind/main.py
@@ -9,6 +9,7 @@ class MasterMind:
     COLORS = ('RED', 'GREEN', 'BLUE', 'YELLOW', 'BLACK', 'WHITE', 'CYAN', 'MAGENTA')
 
     def __init__(self):
+        # Create UI elements
         self.root = tk.Tk()
         self.root.title("Mastermind")
         self.root.option_add('*tearOff', 0)
@@ -22,16 +23,20 @@ class MasterMind:
         self.score_grid = uielements.Grid(self.canvas, 30, self.BOARD_HEIGHT - 520, 10, 1, 50, 50)
         self.check_button = ttk.Button(self.canvas, text="Check!", command=self.validate_line, state='disabled')
 
+        # Create game state variables
         self.base_pegs = {}
         self.code_pegs = []
         self.hint_pegs = []
         self.placed_pegs = [[None, None, None, None] for _ in range(10)]
         self.round = 0
         self.code = None
+
         self.start()
         self.root.mainloop()
 
     def start(self):
+        '''Starts the app by placing UI elements
+        and initialize game'''
         self.root['menu'] = self.menu
         self.menu.add_cascade(menu=self.file_menu, label='File')
         self.file_menu.add_command(label='New game', command=self.reset)
@@ -44,6 +49,7 @@ class MasterMind:
         self.code = self.generate_code()
 
     def reset(self):
+        '''Resets the game for a new round'''
         for row in self.placed_pegs:
             for i, peg in enumerate(row):
                 if isinstance(peg, uielements.Peg):
@@ -62,42 +68,54 @@ class MasterMind:
         self.round = 0
 
     def create_pegs(self):
+        '''Creates the pegs to choose from'''
         for color in self.COLORS:
             self.reset_peg(color)
         self.canvas.tag_raise('peg', 'all')
 
     def reset_peg(self, color):
+        '''Create a new peg after one has been used'''
         self.base_pegs[color] = uielements.Peg(self.canvas, color, \
             self.BOARD_WIDTH - 20, (self.BOARD_HEIGHT - 50) - self.COLORS.index(color) * 30, self)
 
     def place_peg(self, x, y, peg):
+        '''Place a peg on the board'''
         field = self.peg_grid.get_field(x, y)
         self.reset_peg(peg.color)
+        # Check if the peg is already placec (i.e. it's)
+        # moved to a new field, and delete the old reference
         for i in range(len(self.placed_pegs[self.round])):
             if self.placed_pegs[self.round][i] is peg:
                 self.placed_pegs[self.round][i] = None
 
+        # Delete the peg if it's dropped in the wrong place
         if field is None or field['row'] != self.round:
             peg.remove()
         else:
+            # Check if there's already a peg in the field and remove it
             if isinstance(self.placed_pegs[field['row']][field['col']], uielements.Peg):
                 self.placed_pegs[field['row']][field['col']].remove()
+            # Add the peg to the matrix and center it in the field
             self.placed_pegs[field['row']][field['col']] = peg
             peg.move_to(*self.peg_grid.get_field_center(field['row'], field['col']))
 
+        # Enable the check button if the row is full
         if not None in self.placed_pegs[self.round]:
             self.check_button.state(['!disabled'])
         else:
             self.check_button.state(['disabled'])
 
     def validate_line(self):
+        '''Compares the row against the hidden code'''
         score = []
         code = list(self.code)
+        # Check for pegs that are exactly right
         for i, peg in enumerate(self.placed_pegs[self.round]):
             if peg.color == self.code[i]:
                 score.append(2)
                 code[i] = None
 
+        # Check for pegs that have the right color but in the wrong place
         for peg in self.placed_pegs[self.round]:
             if peg.color in code:
                 score.append(1)
@@ -105,6 +123,7 @@ class MasterMind:
 
         self.show_score(score)
 
+        # Check if the game is won or lost
         if score == [2, 2, 2, 2]:
             self.victory()
         elif self.round == 9:
@@ -113,6 +132,7 @@ class MasterMind:
         self.next_round()
 
     def show_score(self, score):
+        '''Place black or white pegs to indicate retult'''
         center_x, center_y = self.score_grid.get_field_center(self.round, 0)
         for i, s in enumerate(score):
             x = center_x - (-1)**i * 10
@@ -123,26 +143,31 @@ class MasterMind:
             self.hint_pegs.append(uielements.draw_circle(self.canvas, x, y, 5, fill=color))
 
     def next_round(self):
+        '''Move on to the next round'''
         for peg in self.placed_pegs[self.round]:
             peg.freeze()
         self.check_button.state(['disabled'])
         self.round += 1
 
     def generate_code(self):
+        '''Genereate a secret code'''
         code = []
         for _ in range(4):
             code.append(random.choice(self.COLORS))
         return tuple(code)
 
     def victory(self):
+        '''The player has won'''
         self.show_code()
         messagebox.showinfo(message='You won!')
 
     def loss(self):
+        '''The player has lost'''
         self.show_code()
         messagebox.showinfo(message='Sorry, you lost!')
 
     def show_code(self):
+        '''Display the correct code'''
         for i, color in enumerate(self.code):
             x = self.peg_grid.get_field_center(0, i)[0]
             peg = uielements.Peg(self.canvas, color, x, 40, self)
--- mastermind/uielements.py
+++ mastermind/uielements.py
@@ -15,22 +15,28 @@ class Peg:
         self._frozen = False
 
     def drag(self, event):
+        '''Move with the mouse cursor'''
         if not self._frozen:
             self.move_to(event.x, event.y)
 
     def drop(self, event):
+        '''Signal to callback that the peg has been dropped
+        and in what position'''
         if not self._frozen:
             self._callback.place_peg(event.x, event.y, self)
 
     def move_to(self, x, y):
+        '''Move to the absolute coordinates'''
         self._canvas.move(self._id, x - self.x, y - self.y)
         self.x = x
         self.y = y
 
     def remove(self):
+        '''Remove the peg from the board'''
         self._canvas.delete(self._id)
 
     def freeze(self):
+        '''Freezes the peg so it can't be moved'''
         self._frozen = True
 
 class Grid():
@@ -46,6 +52,7 @@ class Grid():
         self._w = columns * colwidth
 
     def draw(self):
+        '''Draw a grid according to specs'''
         for col in range(self._cols + 1):
             x = self._x + self._cw * col
             self._canvas.create_line(x, self._y, x, self._y + self._h, width=2, fill='#444444')
@@ -55,13 +62,15 @@ class Grid():
             self._canvas.create_line(self._x, y, self._x + self._w, y, width=2, fill='#444444')
 
     def get_field_center(self, row, col):
+        '''Returns the coordinates of the center of a field'''
         center_x = self._x + (col * self._cw) + self._cw // 2
         center_y = (self._y + self._h) - ((row * self._rh) + self._rh // 2)
 
         return center_x, center_y
 
-
     def get_field(self, x, y):
+        '''Get a field from coordinates, return None if
+        outside the grid'''
         rel_x = x - self._x
         rel_y = y - self._y
         if rel_x < 0 or rel_x > self._w or rel_y < 0 or rel_y > self._h:
@@ -73,10 +82,10 @@ class Grid():
 
 
 def draw_circle(canvas, xpos, ypos, radius, **kwargs):
+    '''Draw a circle centered at (xpos, ypos) with specified radius'''
     x1 = xpos - radius
     y1 = ypos - radius
     x2 = xpos + radius
     y2 = ypos + radius
 
     return canvas.create_oval(x1, y1, x2, y2, **kwargs)
-
