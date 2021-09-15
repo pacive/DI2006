965b89d02550a7f37c07584ce3d3347eca47eed2
Anders Alfredsson <andersb86@gmail.com>
2021-09-15 14:31:18 +0200

    Update prime to calculate prime factors
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>

--- 11_as2/prime.py
+++ 11_as2/prime.py
@@ -1,28 +1,32 @@
 import math
+import sys
 
-def is_prime(num):
+def prime_factors(num, start = 3):
     # Check if num is 2 - the only even prime
     if num == 2:
-        return True
+        return [2]
 
     # If num is even, it can't be a prime
     if num % 2 == 0:
-        return False
+        return [2] + prime_factors(num // 2)
 
     # Only need to test factors up to the sqare root of num
     largest_factor = int(math.sqrt(num))
 
     # If num is divisible by any other number, it's not prime
-    for n in range(3, largest_factor + 1, 2):
+    for n in range(start, largest_factor + 1, 2):
         if num % n == 0:
-            return False
+            return [n] + prime_factors(num // n, n)
 
     # If not divisible by any number, it must be a prime
-    return True
+    return [num]
 
-number = int(input('Skriv ett tal: '))
+if not (len(sys.argv) > 1 and (number := int(sys.argv[1]))):
+    number = int(input('Skriv ett tal: '))
 
-if is_prime(number):
+factors = prime_factors(number)
+
+if len(factors) == 1:
     print(f'{number} är ett primtal')
 else:
-    print(f'{number} är inte ett primtal')
+    print(f'{number} faktoriseras till {factors}')
